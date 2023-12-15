f09e9b3b8781b6110d50150eda841ca6de2b1db9
zhangjingyu <253841389@qq.com>
2023-12-15 17:18:43 +0800

    fix:优化固定值获取方式

--- ruoyi-zhboot/src/main/java/com/ruoyi/homeip/service/impl/TencentCloudServiceImpl.java
+++ ruoyi-zhboot/src/main/java/com/ruoyi/homeip/service/impl/TencentCloudServiceImpl.java
@@ -31,19 +31,15 @@ public class TencentCloudServiceImpl implements ITencentCloudService {
 
     private static final Logger log = LoggerFactory.getLogger(TencentCloudServiceImpl.class);
 
-    private final String SECRET_ID = configService.selectConfigByKey("tencent.cloud.secretId");
-    private final String SECRET_KEY = configService.selectConfigByKey("tencent.cloud.SecretKey");
-
-    private final String API_URL = configService.selectConfigByKey("tencent.cloud.ApiUrl");
 
     private DnspodClient createClient() {
         // 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
         // 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
         // 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-        Credential cred = new Credential(SECRET_ID, SECRET_KEY);
+        Credential cred = new Credential(configService.selectConfigByKey("tencent.cloud.secretId"), configService.selectConfigByKey("tencent.cloud.SecretKey"));
         // 实例化一个http选项，可选的，没有特殊需求可以跳过
         HttpProfile httpProfile = new HttpProfile();
-        httpProfile.setEndpoint(API_URL);
+        httpProfile.setEndpoint(configService.selectConfigByKey("tencent.cloud.ApiUrl"));
         // 实例化一个client选项，可选的，没有特殊需求可以跳过
         ClientProfile clientProfile = new ClientProfile();
         clientProfile.setHttpProfile(httpProfile);
