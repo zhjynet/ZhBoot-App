460db2ecef80126ac20d91ff0a049f6b7dd249ab
zhangjingyu <253841389@qq.com>
2024-07-15 17:47:46 +0800

    feat(*):优化URL

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/homeip/HomeIpController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/homeip/HomeIpController.java
@@ -9,14 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -92,9 +85,8 @@ public class HomeIpController extends BaseController
 
     @ApiOperation("新增IP地址-接口调用")
     @Anonymous
-    @GetMapping("C91FFB1E54CD85E4")
-    public AjaxResult addIp(String homeName)
-    {
+    @RequestMapping(value = "cG9zdG15aG9tZWlwCg", method = {RequestMethod.GET, RequestMethod.POST})
+    public AjaxResult addIp(String homeName) {
         return toAjax(homeIpService.insertHomeIpServlet(homeName));
     }
     /**
